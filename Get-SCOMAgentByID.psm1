d1b916268f8e048ced36a96cc045c43d4de2a3a7
chickentech <complicationalism+github@gmail.com>
2015-11-20 16:34:59 -0500

    New Scripts
    Adding some new scripts that I wrote for work.

---
+++ Get-SCOMAgentByID.psm1
@@ -0,0 +1,37 @@
+﻿#Import-Module OperationsManager
+<#
+By:  Nathan Behe for PA Department of Corrections
+Date:  11/20/2015
+Get-SCOMAgentByID
+#>
+
+Function Get-SCOMAgentByID{
+
+
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+This script will take a GUID and return the computer name that it equates with.
+
+.PARAMETER ID
+This is the GUID of the HealthService Object you would like to get the hostname from.
+
+.EXAMPLE
+Get-SCOMAgentByID -Id d3081202-3d67-995a-66a7-eb6c2f1fc6df
+
+#>
+
+
+[CmdletBinding(DefaultParameterSetName="Id",SupportsShouldProcess=$False,ConfirmImpact='Low')]
+#gather parameters and support pipelining
+Param(
+[parameter(Mandatory=$True,Position=0,HelpMessage="GUID ID of Health Service.")]
+[STRING[]]$Id="test"
+)
+
+$ReturnName = Get-SCOMAgent | where {$_.hostedHealthService.Id -eq $Id[0]} | select name
+
+return $ReturnName
+
+}
---
+++ Telestaff.ps1
@@ -0,0 +1,59 @@
+﻿##
+#
+#
+##
+
+function is64bit() {
+  return ([IntPtr]::Size -eq 8)
+}
+
+$key = 'HKLM:\SOFTWARE\Classes\AppID\{CA12B971-8FC0-405F-A772-FCA2E37ECDF2}'
+$eestore = ((Get-ItemProperty -Path $key).RunAs).ToString()
+
+#IP Addresses
+$QA_IP = "172.22.28.7"
+$DEV_IP = "172.22.28.8"
+$Train_IP = "172.22.28.6"
+$Prod_IP = "172.22.19.190"
+
+while(1){
+Write-Host "Select your environment:"
+Write-Host "1) QA Telestaff Environment"
+Write-Host "2) DEV Telestaff Environment"
+Write-Host "3) Training Telestaff Environment"
+Write-Host "4) PROD Telestaff Environment"
+$Selection = Read-Host "Please enter the number of your selection"
+
+if (($Selection -as [int]) -eq 1 -or 2 -or 3 -or 4){
+    break;
+} else {
+    Write-Host "Invalid Selection.";
+    Start-Sleep 2;
+    }
+}
+
+#Set reg key to chosen environment
+Switch (($Selection -as [int])){
+    1 {Write-Host "Setting Environment to QA"; Set-ItemProperty -Path $key -Name RemoteServerName -Value $QA_IP; break;}
+    2 {Write-Host "Setting Environment to DEV"; Set-ItemProperty -Path $key -Name RemoteServerName -Value $DEV_IP; break;}
+    3 {Write-Host "Setting Environment to Training"; Set-ItemProperty -Path $key -Name RemoteServerName -Value $Train_IP; break;}
+    4 {Write-Host "Setting Enviornment to Production"; Set-ItemProperty -Path $key -Name RemoteServerName -Value $Prod_IP; break;}
+    default { Write-Host "An error has occurred."; exit; break;}
+}
+
+#Determine is x86 or x64
+function get-programfilesdir() {
+  if (is64bit -eq $true) {
+    (Get-Item "Env:ProgramFiles(x86)").Value
+  }
+  else {
+    (Get-Item "Env:ProgramFiles").Value
+  }
+}
+
+$telePath = get-programfilesdir;
+$telePath = $telePath.tostring() + "\Telestaff\client\TeleStaff.exe";
+Write-Host $telePath;
+#Launch Telestaff
+Invoke-Item -Path $telePath;
+Exit;
